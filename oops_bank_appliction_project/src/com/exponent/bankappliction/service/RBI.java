937c5404785962e4e5d671c4e0724af156176c24
NikgilJadhav2004 <nikhiljadhav1080@gmail.com>
2025-05-18 18:54:44 +0530

    Refactored code for better OOP design and readability

---
+++ oops_bank_appliction_project/src/com/exponent/bankappliction/service/RBI.java
@@ -0,0 +1,16 @@
+package com.exponent.bankappliction.service;
+
+public interface RBI {
+	
+	public void registerAccount();
+	public void showAccountDetails();
+	public void showAccountBalance();
+	public void depositMoney();
+	public void withdrawMoney();
+	
+	public void updatesAccountDetails();
+	
+	
+	
+
+}
